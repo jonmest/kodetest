7166eea2aea5d47ff889806ef2be0eafb7c80600
J. William Cavallie Mester <jc222rz@student.lnu.se>
2020-09-19 01:23:47 +0200

    Fetching comments for blog posts. Need to style them later. Done for the night.

--- kodetest/pages/[id].tsx
+++ kodetest/pages/[id].tsx
@@ -1,21 +1,56 @@
-import IPost from '../interfaces'
+import { IPost, IUser, IComment } from '../interfaces'
+import Layout from '../components/layouts'
 
-export default function DetailedPost({ post }: { post: IPost}) {
+type DetailedPostProps = {
+    post: IPost,
+    user: IUser,
+    comments: Array<IComment>
+}
+
+export default function DetailedPost({ post, user, comments }: DetailedPostProps) {
+    const toCapitalized = (str: string) => {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
+
+      console.log(comments)
     return (
-        <p>
-            { post.title }
-        </p>
+        <Layout>
+            <h1>
+                { toCapitalized(post.title) }
+            </h1>
+            <hr/>
+            <p>
+                Written by { user.name }
+            </p>
+            <hr/>
+            <p>
+                { toCapitalized(post.body)}
+            </p>
+
+            <ul>
+                {
+                    comments.map((c: IComment) => {
+                        return <li>{c.body}</li>
+                    })
+                }
+            </ul>
+        </Layout>
     )
 }
 
 export async function getStaticProps({ params }) {
-    const url = `https://jsonplaceholder.typicode.com/posts/${params.id}`
-    const res = await fetch(url)
-    const post: IPost = await res.json()
+    const postRes = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+    const post: IPost = await postRes.json()
+
+    const userRes = await fetch(`https://jsonplaceholder.typicode.com/users/?id=${post.userId}`)
+    const user: IUser = await userRes.json().then(list => list[0])
+
+    const commentsRes = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}/comments`)
+    const comments: Array<IComment> = await commentsRes.json()
 
     return {
         props: {
-        post
+        post, user, comments
         },
     }
 }
